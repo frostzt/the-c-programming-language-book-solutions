eae8be41b4ab7a0db093441499feb70172d1ab48
frostzt <aidenfrostbite@gmail.com>
2021-12-01 17:49:50 +0530

    ex-1.22 complete, still a bit left

--- chapter_one/ex-1.22.c
+++ chapter_one/ex-1.22.c
@@ -7,31 +7,49 @@
  * long lines, and if there are no blanks or tabs before the specified column.
  **/
 
+/**
+ * Idea: In the main function get the characters from the stdin stream, and
+ * store them into a global variable that will hold the entire line. This line
+ * should be calculated to match the MAX_LINE_LENGTH and printed in order so
+ * that a cursor should remain at the lat position.
+ **/
+
 #include <stdio.h>
 
 /*** defines ***/
 
+typedef int bool;
+#define true 1
+#define false 0
+
 #define MAX_LIMIT 5000
-#define MAX_LINE_LENGTH 10
+#define MAX_LINE_LENGTH 50
+
+/*** globals ***/
+
+char line[MAX_LIMIT];
 
 /*** prototypes ***/
 
-int getLineLength(char line[]);
-void printLine(char line[], int length);
+int newposition(int pos);
+void printLineTillNow(int uptoIndex);
 
 /*** main ***/
 
 int main() {
-  char line[MAX_LIMIT];
+  int c, counter;
 
-  while (1) {
-    int length = getLineLength(line);
+  counter = 0;
+  while ((c = getchar()) != EOF) {
+    line[counter] = c;
 
-    if (length == 0) {
-      break;
+    if (c == '\n') {
+      printLineTillNow(counter);
+      counter = 0;
+    } else if (++counter >= MAX_LINE_LENGTH) {
+      printLineTillNow(counter);
+      counter = newposition(counter);
     }
-
-    printLine(line, length);
   }
 
   return 0;
@@ -39,23 +57,22 @@ int main() {
 
 /*** defs ***/
 
-int getLineLength(char line[]) {
-  int c, i;
-
-  c = 0;
-  for (i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
-    line[i] = c;
-  }
-
-  return i;
-}
-
-void printLine(char line[], int length) {
-  for (int i = 0; i < length; i++) {
-    if (i != 0 && i % MAX_LINE_LENGTH == 0) {
-      putchar('\n');
-    }
-
+void printLineTillNow(int uptoIndex) {
+  for (int i = 0; i < uptoIndex; i++) {
     putchar(line[i]);
   }
+  putchar('\n');
+}
+
+int newposition(int pos) {
+  int i, j;
+
+  if (pos <= 0 || pos >= MAX_LINE_LENGTH)
+    return 0;
+  else {
+    i = 0;
+    for (j = pos; j < MAX_LINE_LENGTH; ++j, ++i)
+      line[i] = line[j];
+  }
+  return i;
 }
